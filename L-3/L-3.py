047b9312a1aeaa1cf3ff385810e22c5cb2603fb3
honeyp2242 <honeypatel2242@gmail.com>
2025-06-12 14:40:42 +0530

    Add files via upload

---
+++ L-3/L-3.py
@@ -0,0 +1,13 @@
+# peint() formatting using sep and end
+
+# print("Apple" , "Banana" , "Cherry" , sep=" , " , end="<--- end of list\n")
+
+# formatted message for user input
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# hobby = input("Enter your hobby: ")
+
+# print(" my name is " + name + " and my age is " + age + " and my hobby is " + hobby)
+
+# print(f"Hello , {name}! At {age} , enjoying {hobby} sounds fun!!")
