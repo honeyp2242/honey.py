d65789e8d8270f39d087d6422c17992e150c2dfb
honeyp2242 <honeypatel2242@gmail.com>
2025-06-12 14:41:07 +0530

    Add files via upload

---
+++ L-4/datatype.py
@@ -0,0 +1,109 @@
+#  python datatype , variables and operators
+
+# variables
+
+# x = 5              integer
+# y = "Hello"        string
+# z = 3.14           floting        
+
+# print(x + y + z)
+
+# a = 10
+# b = int("20")
+
+# print(a + b)
+
+# a = str(10)
+# b = "20"
+
+# print(int(a + b))
+
+# x = True
+# y = False
+
+# print("Hello World!!!")
+
+# Arithmetic operators
+
+# + , - , * , / , % , **
+
+# x = 5
+# y = 4
+
+# print(x + y)
+# print(x - y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+
+# Assignment Operator
+
+# x = "hello"
+
+# x = 10
+# y = 20
+
+# x += 20 # x = x + 20 # 30
+# print(x)
+# x -= 20 # x = x - 20 # -10
+# print(x)
+# x *= 5 # x = x * 5  50
+# print(x)
+# x %= 5 # x = x * 5 # 2
+# print(x)
+# x %= 5 # x = x * 5 # 0
+# print(x)
+# Expontiations
+
+# x **=5 # x = x * 5 # 10 x 10 x 10 x 10 x 10
+
+# print (x)
+
+# condition operators
+
+# == , === , < , > , <= , >=
+
+# x = "hello"
+# y = "hello"
+
+# print(x == y)
+
+# x = 20
+# y = 20
+
+# print(x == y)
+
+# x = "20"
+# y = "20"
+
+# print(x == y)
+# x = 20
+# y = -20
+
+# print(x ==y)
+
+# x = 20
+# y = 20
+
+# print(x < y)
+# print(x > y)
+# print(x >= y)
+# print(x <= y)
+
+# x , y , z = 10 , 20 , 30
+
+# print(x * z)
+# print(y - x)
+# print(z + y)
+
+# logical operators
+
+# x = True
+# y = False
+# Z = False
+
+# print(x or y or z)
+
+# print(x and y and z)
+
+# print(not x)
