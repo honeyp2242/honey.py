d5115b6428e5cfe2274fc4c0ae8285eee330bdda
honeyp2242 <honeypatel2242@gmail.com>
2025-06-12 14:39:22 +0530

    Add files via upload

---
+++ H.W.py/loop-3-H.W-2.py
@@ -0,0 +1,41 @@
+1 Pattern Print in python
+
+for i in range(1 , 7):
+    for j in range(1 , i):
+        print(f"{j}" , end="")
+    print()
+
+2 Pattern Print in python
+
+for i in range(5):
+    for j in range(i , -1 ,-1):
+        print(j+1 , end="")
+    print()
+
+3 Pattern Print in python
+
+for i in range(1 , 6):
+    for j in range(i):
+        print(f"{i}" , end="")
+    print()
+
+4 Pattern Print in python
+
+for i in range(5):
+    for j in range(i+1):
+        print(5-i, end="")
+    print()
+
+5 Pattern Print in python
+
+for i in range(5):
+    for j in range(i , -1 , -1):
+        print(5-j , end="")
+    print()
+
+6 Pattern Print in python
+
+for i in range(5):
+    for j in range(i+1):
+        print(5-j, end ="")
+    print()
