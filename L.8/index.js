25f79fed6530b448c014448ddc4bd3c4d9ecb9d6
honeyp2242 <honeypatel2242@gmail.com>
2025-06-12 14:42:16 +0530

    Add files via upload

---
+++ L.8/index.js
@@ -0,0 +1,3 @@
+var str1 = "hello" + 10
+
+console.log(str1);
---
+++ L.8/pattern.py
@@ -0,0 +1,43 @@
+# * pattern in python
+# n = 7
+# i = 0 , 1 , 2 , 3 , 4
+# for i in range(n):
+#     print(i)
+#     for j in range(i):
+#         print('*' , end="")
+#     print()
+
+# python number pattern
+
+# for i in range(6):
+#   for j in range(1 , i + 1):
+#     print(j , end=" ")
+# print()
+
+# # String
+
+# str1 = "Hello" + "Wrold"
+# str2 = "Hello" * 10
+# print(str2)
+
+# # pattern 2
+
+# for i in range(1 , 6):
+#     for j in range(i , 0 , -1):
+#         print(j , end=" ")
+#     print()
+
+# # patter 3
+
+# for i in range(1 , 6):
+#     print((str(i) + ' ') * (5 - i + 1))
+
+# # pattern 4
+
+# # Diamond Pattern
+
+# for i in range(5):
+#     print(' ' * (5 - i -1) + '* ' * (i + 1))
+
+# for i in range(5 - 2 , -1 , -1):
+#     print(' ' * (5 - i - 1) + '* ' * (i + 1))
