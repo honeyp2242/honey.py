44435a1c187906644e921e78c5fce3c5bd77f855
honeyp2242 <honeypatel2242@gmail.com>
2025-06-12 14:39:54 +0530

    Add files via upload

---
+++ L-1/index.py.py
@@ -0,0 +1,16 @@
+#print Hello World Program
+
+# print("Hello World!")
+
+#print your name
+# name = input("Enter Your Name :")
+# print('Hello' , name)
+
+#check your age is 18+ so you are eligible for voting or your age is 18- so you are not eligible for voting
+
+# num = int(input("Enter your age:"))
+
+# if num > 18:
+#     print("you are eligible for voting")
+# elif num < 18:
+#     print("you not are eligible for voting")
